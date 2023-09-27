28c91914ae877fae5af209c4838a8d985ee34cef
Tlholo Koma <tlholo@bbd.co.za>
2023-09-27 21:35:39 +0200

    fix

--- calculator_quiz/src/main/java/com/testing/calculator_quiz/controller/CalculatorController.java
+++ calculator_quiz/src/main/java/com/testing/calculator_quiz/controller/CalculatorController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.testing.calculator_quiz.service.Calculator;
+
 import lombok.RequiredArgsConstructor;
-import service.Calculator;
 
 import java.util.Map;
 
