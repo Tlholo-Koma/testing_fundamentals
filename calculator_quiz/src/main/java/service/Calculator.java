e241396cc6b4cfa31396cb58f41d830c1d4a37fb
Tlholo Koma <tlholo@bbd.co.za>
2023-09-27 19:23:30 +0200

    fixes

--- calculator_quiz/src/main/java/service/Calculator.java
+++ calculator_quiz/src/main/java/service/Calculator.java
@@ -18,6 +18,8 @@ public class Calculator {
         operatorMap.put('*', new Multiplication());
         operatorMap.put('/', new Division());
         operatorMap.put('^', new Power());
+        operatorMap.put(')', null);
+        operatorMap.put('(', null);
     }
 
     public Double calculate(String expression) throws Exception{
@@ -29,8 +31,9 @@ public class Calculator {
                 char exp = postfix.charAt(i);
                 if (exp == '$') {
                     operands.push(Double.valueOf(operand));
+                    operand = "";
                 } else if (exp >= '0' && exp <= '9') {
-                    postfix += exp;
+                    operand += exp;
                 } else {
                     value1 = operands.pop();
                     value2 = operands.pop(); 
@@ -52,7 +55,7 @@ public class Calculator {
         try {
             for (int i = 0; i < expression.length(); i++) {
                 char exp = expression.charAt(i);
-                if (i != 0 && operatorMap.containsKey(exp))
+                if (i != 0 && operatorMap.containsKey(exp) && !operatorMap.containsKey(postfix.charAt(postfix.length()-1)))
                     postfix += "$";
                 
                 if (exp >= '0' && exp <= '9') {
@@ -61,20 +64,21 @@ public class Calculator {
                     operators.push(exp);
                 } else if (exp == ')') {
                     while (!operators.isEmpty() && operators.peek() != '(') {
-                        postfix += operands.pop();
+                        postfix += operators.pop();
                     }
                     if (!operators.isEmpty()) {
                         operators.pop();
                     }
                 }
                 else {
-                    postfix += "$";
                     while (!operators.isEmpty() && operatorPrecedence(operators.peek()) >= operatorPrecedence(exp)) {
                         postfix += operators.pop();
                     }
                     operators.push(exp);
                 }
             }
+            if (!operatorMap.containsKey(expression.charAt(expression.length()-1)))
+                postfix += "$";
             while (!operators.isEmpty())
                 postfix += operators.pop();
             return postfix;
