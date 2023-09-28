e8c31b87c3e7aa65fcb6bb15559a7fffa22020fb
Xander <xlubbe@gmail.com>
2023-09-28 21:10:53 +0200

    Test for get and post for ScoreController working

--- server/calculator_quiz/src/test/java/com/testing/calculator_quiz/ScoreControllerIntegrationTest.java
+++ server/calculator_quiz/src/test/java/com/testing/calculator_quiz/ScoreControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.testing.calculator_quiz;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,12 +14,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -34,10 +33,24 @@ public class ScoreControllerIntegrationTest {
     }
 
     @Test
-    public void testCalculation() throws JsonProcessingException, Exception {
+    public void testGetUserScore() throws JsonProcessingException, Exception {
 
         mvc.perform(get("/user/6/score"))
             .andExpect(status().isOk())
             .andReturn();
+    }
+    @Test
+    public void testPostUserScore() throws JsonProcessingException, Exception {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode requestBody = objectMapper.createObjectNode();
+        requestBody.put("email", "example@email.com");
+
+
+        mvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody.toString()))
+                .andExpect(status().isOk())
+                .andReturn();
     }
 }
