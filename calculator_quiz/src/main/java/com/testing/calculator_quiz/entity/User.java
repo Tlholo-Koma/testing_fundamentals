bc7d13f5eeef98f625acaa140bc47ed34808ddb8
Xander <xlubbe@gmail.com>
2023-09-28 11:41:37 +0200

    Get for Score and User, Post not working yet

--- calculator_quiz/src/main/java/com/testing/calculator_quiz/entity/User.java
+++ calculator_quiz/src/main/java/com/testing/calculator_quiz/entity/User.java
@@ -3,19 +3,16 @@ package com.testing.calculator_quiz.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 
-@Entity(name = "SCORE")
-@Table(name = "SCORE")
+@Entity(name = "USERS")
+@Table(name = "USERS")
 @Getter
-public class Score {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long Id;
     private int userId;
-    private int score;
-    @Column(name = "SCORE_DATE_TIME")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date scoreDateTime;
+    private String email;
 }
+
---
+++ calculator_quiz/src/main/java/com/testing/calculator_quiz/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.testing.calculator_quiz.repository;
+
+import com.testing.calculator_quiz.entity.Score;
+import com.testing.calculator_quiz.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository  extends CrudRepository<User, Integer> {
+
+    @Query("SELECT t FROM USERS t WHERE t.userId = :userId")
+    User getUser(int userId);
+
+//    @Query("SELECT t FROM USERS t WHERE t.userId = :userId")
+//    User setUser(int userId);
+}
